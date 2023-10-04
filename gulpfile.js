fa310d05ea2acb9e0a9241aee53b1d350659761c
develope-of-rivia <samping2money@gmail.com>
2023-10-04 10:54:21 +0700

    сделано версионирование файлов

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-let folderName = 'vk';
+let folderName = 'antibot-ad';
 
 let preprocessor = 'less', // Preprocessor (sass, less, styl); 'sass' also work with the Scss syntax in blocks/ folder.
 	fileswatch   = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
 
 import pkg from 'gulp'
-const { gulp, src, dest, parallel, series, watch } = pkg
+const { gulp, src, dest, parallel, series, watch, task } = pkg
 
 import browserSync   from 'browser-sync'
 import bssi          from 'browsersync-ssi'
@@ -13,6 +13,7 @@ import webpackStream from 'webpack-stream'
 import webpack       from 'webpack'
 import TerserPlugin  from 'terser-webpack-plugin'
 import gulpSass      from 'gulp-sass'
+import htmlreplace from 'gulp-html-replace'
 import dartSass      from 'sass'
 import sassglob      from 'gulp-sass-glob'
 const  sass          = gulpSass(dartSass)
@@ -119,6 +120,61 @@ async function buildhtml() {
 	await deleteAsync(`${folderName}/dist/parts`, { force: true })
 }
 
+
+
+/* неумело сделанное версионирование файлов */
+function cssFileVersion(){
+	let versionNum = new Date(),
+	versionNumMonth = versionNum.getMonth(),
+	versionNumDay = versionNum.getDate(),
+	versionNumHours = versionNum.getHours(),
+	versionNumMin = versionNum.getMinutes(),
+	versionNumSec = versionNum.getSeconds();
+	versionNum = versionNumMonth.toString() + versionNumDay.toString() + versionNumHours.toString() + versionNumMin.toString() + versionNumSec.toString();
+
+	return src(`${folderName}/dist/css/app.min.css`)
+	.pipe(concat(`app.min.${versionNum}.css`))
+	.pipe(dest(`${folderName}/dist/css`))
+}
+function jsFileVersion(){
+	let versionNum = new Date(),
+	versionNumMonth = versionNum.getMonth(),
+	versionNumDay = versionNum.getDate(),
+	versionNumHours = versionNum.getHours(),
+	versionNumMin = versionNum.getMinutes(),
+	versionNumSec = versionNum.getSeconds();
+	versionNum = versionNumMonth.toString() + versionNumDay.toString() + versionNumHours.toString() + versionNumMin.toString() + versionNumSec.toString();
+
+	return src(`${folderName}/dist/js/app.min.js`)
+	.pipe(concat(`app.min.${versionNum}.js`))
+	.pipe(dest(`${folderName}/dist/js`))
+}
+function filesVersionHtml(){
+	let versionNum = new Date(),
+	versionNumMonth = versionNum.getMonth(),
+	versionNumDay = versionNum.getDate(),
+	versionNumHours = versionNum.getHours(),
+	versionNumMin = versionNum.getMinutes(),
+	versionNumSec = versionNum.getSeconds();
+	versionNum = versionNumMonth.toString() + versionNumDay.toString() + versionNumHours.toString() + versionNumMin.toString() + versionNumSec.toString();
+
+	return src(`${folderName}/dist/index.html`)
+    .pipe(htmlreplace({
+        'css': `css/app.min.${versionNum}.css`,
+		'js': `js/app.min.${versionNum}.js`
+    }))
+    .pipe(dest(`${folderName}/dist/`));
+};
+async function deleteOldCss() {
+	await deleteAsync(`${folderName}/dist/css/app.min.css`, { force: true })
+}
+async function deleteOldJs() {
+	await deleteAsync(`${folderName}/dist/js/app.min.js`, { force: true })
+}
+/**/
+
+
+
 async function cleandist() {
 	await deleteAsync(`${folderName}/dist/**/*`, { force: true })
 }
@@ -148,6 +204,6 @@ function startwatch() {
 
 export { scripts, styles, images, deploy }
 export let assets = series(scripts, styles, images)
-export let build = series(cleandist, images, scripts, styles, buildcopy, buildhtml)
+export let build = series(cleandist, images, scripts, styles, buildcopy, buildhtml, cssFileVersion, jsFileVersion, filesVersionHtml, deleteOldCss, deleteOldJs)
 
 export default series(scripts, styles, images, parallel(browsersync, startwatch))
