ad92f84bf8f31dc4191bd3751cfad74ccd280110
develope-of-rivia <samping2money@gmail.com>
2023-10-21 09:48:55 +0700

    captcha немного изображений заменено

---
+++ captcha — копия/dist/js/app.min.919144415.js
@@ -0,0 +1,161 @@
+document.addEventListener('DOMContentLoaded', () => {
+	// menu
+	let burgerIcon = document.querySelector('.header__toggle-burger');
+	let closeIcon = document.querySelector('.header__toggle-close');
+	let menu = document.querySelector('.header__panel');
+	let scrollObject = document.querySelector('.header__panel');
+	burgerIcon.addEventListener('click', function(){
+		closeIcon.classList.remove('hidden');
+		burgerIcon.classList.add('hidden');
+		menu.classList.add('header__panel--open');
+		// scrollLock.disablePageScroll(scrollObject);
+	})
+	closeIcon.addEventListener('click', function(){
+		closeIcon.classList.add('hidden');
+		burgerIcon.classList.remove('hidden');
+		menu.classList.remove('header__panel--open');
+		// scrollLock.enablePageScroll(scrollObject);
+	})
+
+
+	// dropdown
+	let dropdowns = document.querySelectorAll('.nav__dropdown');
+	dropdowns.forEach(dropdown => {
+		document.addEventListener('click', (e) => {
+			if(!e.composedPath().includes(dropdown)){
+				dropdown.classList.remove('nav__dropdown--open');
+			}
+		});
+		dropdown.addEventListener('click', function(){
+			dropdown.classList.toggle('nav__dropdown--open');
+		});
+	});
+
+
+	// accordion
+	const accordions = document.querySelectorAll('.accordion');
+	accordions.forEach(el => {
+		el.addEventListener('click', (e) => {
+			const self = e.currentTarget;
+			const control = self.querySelector('.accordion__control');
+			const content = self.querySelector('.accordion__content');
+
+			self.classList.toggle('open');
+
+			// если открыт аккордеон
+			if (self.classList.contains('open')) {
+				control.setAttribute('aria-expanded', true);
+				content.setAttribute('aria-hidden', false);
+				content.style.maxHeight = content.scrollHeight + 'px';
+			} else {
+				control.setAttribute('aria-expanded', false);
+				content.setAttribute('aria-hidden', true);
+				content.style.maxHeight = null;
+			}
+		});
+	});
+
+
+
+	// lazymap
+	function lazyScroll() {
+		loadMapBlock.classList.contains("_loaded") || getMap()
+	}
+	window.addEventListener("scroll", lazyScroll);
+	const windowHeight = document.documentElement.clientHeight,
+		loadMapBlock = document.querySelector(".lazy-iframe");
+	
+	function getMap() {
+		const e = loadMapBlock.getBoundingClientRect().top + window.pageYOffset;
+		if (window.pageYOffset > e - windowHeight) {
+			const e = loadMapBlock.dataset.map;
+			loadMapBlock.insertAdjacentHTML("beforeend", `<iframe src="${e}" style="border: 0" allowfullscreen=""></iframe>`), loadMapBlock.classList.add("_loaded")
+		}
+	}
+	loadMapBlock.classList.contains("_loaded") || getMap();
+})
+// slider
+// slider
+// slider
+const slider = document.querySelector('.slider');
+const before = document.querySelector('.before');
+const beforeImage = before.querySelector('img');
+const change = document.querySelector('.change');
+const body = document.body;
+
+let isActive = false;
+
+document.addEventListener('DOMContentLoaded', () => {
+	let width = slider.offsetWidth;
+	beforeImage.style.width = `${width}px`;
+	beforeImage.style.minWidth = `${width}px`;
+});
+
+change.addEventListener('mousedown', () => {
+	isActive = true;
+});
+
+body.addEventListener('mouseup', () => {
+	isActive = false;
+});
+
+body.addEventListener('mouseleave', () => {
+	isActive = false;
+});
+
+const beforeAfterSlider = (x) => {
+	let shift = Math.max(0, Math.min(x, slider.offsetWidth));
+	before.style.width = `${shift}px`;
+	change.style.left = `${shift}px`;
+
+	if(change.style.left < '180px'){
+		console.log(change.style.left);
+	}
+};
+
+const pauseEvents = (e) => {
+	e.stopPropagation();
+	e.preventDefault();
+	return false;
+};
+
+body.addEventListener('mousemove', (e) => {
+	if (!isActive) {
+		return;
+	}
+
+	let x = e.pageX;
+	x -= slider.getBoundingClientRect().left;
+	beforeAfterSlider(x);
+	pauseEvents(e);
+});
+
+change.addEventListener('touchstart', () => {
+	isActive = true;
+});
+
+body.addEventListener('touchend', () => {
+	isActive = false;
+});
+
+body.addEventListener('touchcancel', () => {
+	isActive = false;
+});
+
+body.addEventListener('touchmove', (e) => {
+	if (!isActive) {
+		return;
+	}
+
+let x;
+
+let i;
+for (i = 0; i < e.changedTouches.length; i++) {
+	x = e.changedTouches[i].pageX; 
+}
+
+x -= slider.getBoundingClientRect().left;
+
+beforeAfterSlider(x);
+pauseEvents(e);
+});
