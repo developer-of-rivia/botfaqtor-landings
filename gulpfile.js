1608bad58501d1693ea9c79018b77fc3d2975c3f
develope-of-rivia <samping2money@gmail.com>
2023-10-22 19:44:41 +0700

    заменена шапка у ленднигов

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-let folderName = 'vk';
+let folderName = 'captcha';
 
 let preprocessor = 'less', // Preprocessor (sass, less, styl); 'sass' also work with the Scss syntax in blocks/ folder.
 	fileswatch   = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
